f52e2f090d4983c71d5c0a08494edf79d8c97006
yunfei.fan <fyf786452470@gmail.com>
2021-09-27 18:23:35 +0800

    删除exampe中的pods

--- Example/Pods/Target Support Files/Pods-FYFTableCollectionMiddleWare_Example/Pods-FYFTableCollectionMiddleWare_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FYFTableCollectionMiddleWare_Example : NSObject
-@end
-@implementation PodsDummy_Pods_FYFTableCollectionMiddleWare_Example
-@end
--- Example/Pods/Target Support Files/Pods-FYFTableCollectionMiddleWare_Tests/Pods-FYFTableCollectionMiddleWare_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FYFTableCollectionMiddleWare_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_FYFTableCollectionMiddleWare_Tests
-@end
