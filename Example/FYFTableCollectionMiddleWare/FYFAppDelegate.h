368b25dd9c452c9a56c90ca7ea261d67c894c167
yunfei.fan <fyf786452470@gmail.com>
2021-09-27 18:13:36 +0800

    Initial commit

---
+++ Example/FYFTableCollectionMiddleWare/FYFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FYFAppDelegate.h
+//  FYFTableCollectionMiddleWare
+//
+//  Created by 786452470@qq.com on 09/27/2021.
+//  Copyright (c) 2021 786452470@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FYFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/FYFTableCollectionMiddleWare/FYFViewController.h
@@ -0,0 +1,13 @@
+//
+//  FYFViewController.h
+//  FYFTableCollectionMiddleWare
+//
+//  Created by 786452470@qq.com on 09/27/2021.
+//  Copyright (c) 2021 786452470@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FYFViewController : UIViewController
+
+@end
